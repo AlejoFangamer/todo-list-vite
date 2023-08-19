2e51d4ebf4b2a4f09d0147125bec56e929c3a748
AlejoFangamer <xxalejoxx71@gmail.com>
2023-08-19 11:23:50 -0500

    Arreglitos

--- public/js/obtenerTareas.js
+++ public/js/obtenerTareas.js
@@ -4,12 +4,12 @@ const lista = document.querySelector("#lista");
 
 export async function obtenerTareas(rango1, rango2) {
   try {
-    for (let i = rango1; i <= rango2; i++) {
-      const respuesta = await fetch(
-        `https://jsonplaceholder.typicode.com/todos/${i}`
-      );
-      const data = await respuesta.json();
-      crearTarea(lista, data.title, data.completed);
+    const respuesta = await fetch(
+      `https://jsonplaceholder.typicode.com/todos/`
+    );
+    const data = await respuesta.json();
+    for (let i = rango1 - 1; i <= rango2 - 1; i++) {
+      crearTarea(lista, data[i].title, data[i].completed);
     }
   } catch (e) {
     console.log(e);
